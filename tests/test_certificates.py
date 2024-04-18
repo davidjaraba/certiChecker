57c5389d49ff65475461ce5ebdad0757a3399a5a
davidjaraba <velcas669@gmail.com>
2024-04-18 09:44:24 +0200

    Agregados tests para el post

--- tests/test_certificates.py
+++ tests/test_certificates.py
@@ -8,6 +8,13 @@ def make_request(path: str):
     return response
 
 
+def make_post(path: str, body):
+    base_url = "http://localhost:8000/api"
+    response = requests.post(f'{base_url + path}', json=body)
+    return response
+
+
+
 def test_get_by_id_success():
     response = make_request("/certificates/1")
 
@@ -29,3 +36,26 @@ def test_get_by_id_error():
     response = make_request("/certificates/asdasdasd")
 
     assert response.status_code == 422
+
+
+def test_create():
+    body = {'name': "prueba"}
+    response = make_post("/certificates", body)
+    assert response.status_code == 201
+    assert response.json()["name"] == "prueba"
+
+
+def test_create_error_badrequest():
+    body = {'namjhjje': "prueba"}
+    response = make_post("/certificates", body)
+    assert response.status_code == 400
+
+
+def test_create_error_confict():
+    body = {'name': "prueba"}
+    make_post("/certificates", body)
+    response = make_post("/certificates", body)
+    assert response.status_code == 409
+
+
+
