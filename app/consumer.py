603a7908d08e28e4604a7e57f33b7c81f7f56411
davidjaraba <velcas669@gmail.com>
2024-06-04 11:43:04 +0200

    reduces workers

--- app/consumer.py
+++ app/consumer.py
@@ -51,7 +51,7 @@ def add_url_to_queue(queue, url: str, origin_url: str, depth=default_depth):
 def consumer_handler(queue):
     """Función consumidora que maneja los elementos de la cola."""
 
-    with Pool(processes=3) as pool:
+    with Pool(processes=1) as pool:
         while True:
             try:
                 print('Elementos actualmente en la cola =>>> '+str(queue.qsize()))
